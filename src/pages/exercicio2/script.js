fc3c1d50e1c1a00da27883c8ece0d26abb0f5092
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-20 22:03:56 -0300

    Ending exercise two

---
+++ src/pages/exercicio2/script.js
@@ -0,0 +1,40 @@
+let vetor = [];
+
+document.getElementById("clickMe").onclick = addNumber;
+var inputValue = document.getElementById("inputArray");
+var inputString = document.getElementById("inputString");
+var ul = document.createElement("list").innerHTML;
+
+function addNumber(){
+    if(inputValue.value !== ""){
+        vetor.push(inputValue.value);
+        ul = ul +"<tr><td>"+inputValue.value+"</td></tr>";
+        document.getElementById("list").innerHTML = ul;
+    }
+    else{
+        alert("Insira algum item no vetor")
+    }
+}
+ 
+function fixString(){
+    document.getElementById("inputStringResult").innerHTML = inputString.value;
+}
+
+const arrayContains = (vetor, string) => {  
+
+    // let data = vetor.filter((valor)=> {
+    //     valor === string
+    // })
+        // if(data.lenght > 0){
+        //     alert("true", `vetor:${vetor} string: ${string}`);
+        // }
+        // else{
+
+        //     alert("false");
+        // }
+    vetor.forEach(element => {
+        if(string == element){
+            document.getElementById("contido").innerHTML = "Esta contido"
+        }
+    })
+}
