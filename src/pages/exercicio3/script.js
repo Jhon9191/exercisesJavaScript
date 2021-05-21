af8bf6b5eb775165b4ac2d6de5b16066c0241e1e
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-20 23:41:27 -0300

    Finishing exercise three

--- src/pages/exercicio3/script.js
+++ src/pages/exercicio3/script.js
@@ -3,31 +3,39 @@ let vetor = [];
 document.getElementById("clickMe").onclick = addNumber;
 var inputValue = document.getElementById("inputArray");
 var inputString = document.getElementById("inputString");
+var inputRepeats = document.getElementById("valueForRepeats");
 var ul = document.createElement("list").innerHTML;
 
-function addNumber(){
-    if(inputValue.value !== ""){
+function addNumber() {
+    if (inputValue.value !== "") {
         vetor.push(inputValue.value);
-        ul = ul +"<tr><td>"+inputValue.value+"</td></tr>";
+        ul = ul + "<tr><td>" + inputValue.value + "</td></tr>";
         document.getElementById("list").innerHTML = ul;
     }
-    else{
+    else {
         alert("Insira algum item no vetor")
     }
 }
- 
-function fixString(){
-    document.getElementById("inputStringResult").innerHTML = inputString.value;
-}
 
-const arrayContains = (vetor, string) => {  
-
+const arrayContains = (vetor, string) => {
+    
     let data = vetor.filter((item) => item == string);
     console.log(`data: ${data}`)
+    
+    if (data.length >= 2) {
+        document.getElementById("contido").innerHTML = "Sim"
+    } else {
+        document.getElementById("contido").innerHTML = "Não"
+    }
+}
 
-        if(data.length >=2){
-            document.getElementById("contido").innerHTML = "Sim"
-        }else{
-            document.getElementById("contido").innerHTML = "Não"
-        }
+const arrayContainsTree = (vetor, string, number) => {
+    let data = vetor.filter((item) => item == string);
+    console.log(`data: ${data}`)
+    
+    if (data.length == number) {
+        document.getElementById("contido").innerHTML = "Sim"
+    } else {
+        document.getElementById("contido").innerHTML = "Não"
+    }
 }
