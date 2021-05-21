504c2dfdfff5a32119cd4b255ba4f34142108970
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-20 22:44:40 -0300

    Finishing first part of exercise three

---
+++ src/pages/exercicio3/script.js
@@ -0,0 +1,35 @@
+let vetor = [];
+
+document.getElementById("clickMe").onclick = addNumber;
+var inputValue = document.getElementById("inputArray");
+var inputString = document.getElementById("inputString");
+var ul = document.createElement("list").innerHTML;
+
+function addNumber(){
+    if(inputValue.value !== ""){
+        vetor.push(inputValue.value);
+        ul = ul +"<tr><td>"+inputValue.value+"</td></tr>";
+        document.getElementById("list").innerHTML = ul;
+    }
+    else{
+        alert("Insira algum item no vetor")
+    }
+}
+ 
+function fixString(){
+    document.getElementById("inputStringResult").innerHTML = inputString.value;
+}
+
+const arrayContains = (vetor, string) => {  
+
+    let data = vetor.filter((item) => item == string);
+    console.log(`data: ${data}`)
+
+    console.log(vetor, string)
+
+        if(data.length >=2){
+            document.getElementById("contido").innerHTML = "Esta contido"
+        }else{
+            document.getElementById("contido").innerHTML = "Não esta contido"
+        }
+}
